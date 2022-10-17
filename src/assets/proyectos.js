44d2f04529065230491be07d2e63dcd3f3cbf295
cerveux <diegomartinguzman@hotmail.com.ar>
2022-10-16 21:46:57 -0300

    prueba

---
+++ src/assets/proyectos.js
@@ -0,0 +1,47 @@
+const proyectos = {
+    propios: [
+        {
+            imagen: "https://raw.githubusercontent.com/cerveux/csc_renders/main/src/media/screenshot/csc.jpg",
+            link: "https://csc-renders.vercel.app/",
+            repositorio: "https://github.com/cerveux/csc_renders",
+            titulo: "CSC Renders"
+        },
+        {
+            imagen: "https://raw.githubusercontent.com/cerveux/melirestful/main/screenshot/screenshot-1.jpg",
+            link: "https://meliresful.herokuapp.com",
+            repositorio: "",
+            titulo: "Práctica API MELI"
+        }
+    ],
+    udemy: [
+        {
+            imagen: "https://raw.githubusercontent.com/cerveux/Keeper/main/screenshot/screenshot.jpg",
+            link: "https://keeper-cerveux.herokuapp.com/",
+            repositorio: "https://github.com/cerveux/Keeper",
+            titulo: "Keeper App"
+        }
+
+    ],
+    oracle: [
+        {
+            imagen: "https://raw.githubusercontent.com/cerveux/Challenge-ONE-Sprint-01/main/imagenes/screens/challenge-1.jpg",
+            link: "https://cerveux.github.io/Challenge-ONE-Sprint-01/",
+            repositorio: "https://github.com/cerveux/Challenge-ONE-Sprint-01",
+            titulo: "Decodificador de Texto"
+        },
+        {
+            imagen: "https://raw.githubusercontent.com/cerveux/Challenge-ONE-Sprint-02/main/imagenes/screens/challenge-2.jpg",
+            link: "https://cerveux.github.io/Challenge-ONE-Sprint-02/",
+            repositorio: "https://github.com/cerveux/Challenge-ONE-Sprint-02",
+            titulo: "Juego del Ahorcado"
+        },
+        {
+            imagen: "https://raw.githubusercontent.com/cerveux/Challenge-ONE-Sprint-02/main/imagenes/screens/challenge-2.jpg",
+            link: "https://cerveux.github.io/Challenge-ONE-Sprint-02/",
+            repositorio: "https://github.com/cerveux/Challenge-ONE-Sprint-02",
+            titulo: "Juego del Ahorcado"
+        }
+    ]
+}
+
+export default proyectos;
